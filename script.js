ac096e716acb60fbb9e5c5622dcb6d63cc91f4d5
SuriyaJ <suriyajaganathan42@gmail.com>
2021-08-07 10:14:57 +0530

    commit

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-var height = parseFloat(prompt("enter the height in meters:"));
-var weight = parseFloat(prompt("enter the weight in kg:"));
+function calc() {
+    var height = parseFloat(document.getElementById("in1").value);
+    var weight = parseFloat(document.getElementById("in2").value);
+    var bmi = 0;
 
-if (height == "")
-    document.write("Provide a valid Height!");
-
-else if (weight == "")
-    document.write("Provide a valid Weight!");
-else {
-
-
-    var bmi = weight / (height * height);
+    let range1 = document.getElementById("range1").value;
+    let range2 = document.getElementById("range2").value;
+    if (range2 == "lbs") {
+        weight = weight / 2.2;
+    }
+    if (range1 == "cm") {
+        bmi = (weight / (height * height) / 10000);
+    } else {
+        bmi = weight / (height * height);
+    }
 
 
     if (bmi < 18.6) {
-        console.log(bmi + ":Underweight")
-        document.write(bmi + ":underweight");
+        document.getElementById('out').value = bmi;
+        document.getElementById('div').innerText = "under weight";
     } else if (bmi >= 18.6 && bmi < 24.9) {
-        console.log(bmi + ":normal range")
-        document.write(bmi + ":normal range");
+        document.getElementById('out').value = bmi;
+        document.getElementById('div').innerText = "normal range";
     } else if (bmi >= 25.0 && bmi < 29.9) {
-        console.log(bmi + ":Over weight")
-        document.write(bmi + ":Over weight");
+        document.getElementById('out').value = bmi;
+        document.getElementById('div').innerText = "over weight";
     } else if (bmi >= 30.0 && bmi < 34.9) {
-        console.log(bmi + ":obse class 1")
-        document.write(bmi + ":obse class 1");
+        document.getElementById('out').value = bmi;
+        document.getElementById('div').innerText = "obse class 1";
     } else if (bmi >= 35.0 && bmi < 39.9) {
-        console.log(bmi + ":obse class 2")
-        document.write(bmi + ":obse class 2");
+        document.getElementById('out').value = bmi;
+        document.getElementById('div').innerText = "obse class 2";
     } else if (bmi >= 40.0) {
-        console.log(bmi + ":obse class 3")
-        document.write(bmi + ":obse class 3");
+        document.getElementById('out').value = bmi;
+        document.getElementById('div').innerText = "obse class 3";
     }
+}
+
+function reset() {
+    document.getElementById("in1").value = "";
+    document.getElementById("in2").value = "";
+    document.getElementById("out").value = "";
+    document.getElementById("div").innerText = "";
 }
